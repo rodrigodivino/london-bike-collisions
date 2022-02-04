a9fe488cd4b7cc0dabc2f7f83bd1fd1d95077e06
Rodrigo Divino <rodrigo.sad.lima@gmail.com>
2022-02-04 09:52:11 -0300

    Initial setup

---
+++ components/leaflet-map/leaflet-map.tsx
@@ -0,0 +1,11 @@
+import {FunctionComponent} from "react";
+import {LeafletMap} from "./leaflet-map.types";
+import styles from './leaflet-map.module.css';
+
+const LeafletMap: FunctionComponent<LeafletMap.Props> = () => {
+  return <div className={styles.container}>
+  
+  </div>
+}
+
+export default LeafletMap;
---
+++ components/leaflet-map/leaflet-map.types.ts
@@ -0,0 +1,5 @@
+export namespace LeafletMap {
+  export interface Props {
+  
+  }
+}
