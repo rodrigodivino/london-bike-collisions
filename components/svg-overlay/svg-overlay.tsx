e9e7082c46a893e4d3de971a300ebaa264d7a0a5
Rodrigo Divino <rodrigo.sad.lima@gmail.com>
2022-02-14 11:07:58 -0300

    Move optimization to render method

--- components/svg-overlay/svg-overlay.tsx
+++ components/svg-overlay/svg-overlay.tsx
@@ -5,8 +5,7 @@ import {CollisionSeverity} from "../../types/collision-severity";
 import {BikeCollision} from "../../types/bike-collision";
 import {getProjectedLayout, ProjectedLayout} from "../../hooks/get-projected-layout";
 import {hexbin} from "d3-hexbin";
-import {extent, min, scaleLinear} from "d3";
-import {getMetersInPixels} from "../../hooks/get-meters-in-pixels";
+import {extent, max, scaleLinear} from "d3";
 
 const SVGOverlay: FunctionComponent<SVGOverlayTypes.Props> = ({map, data, isZooming}) => {
   const projectedData = getProjectedLayout<BikeCollision>(
@@ -14,53 +13,65 @@ const SVGOverlay: FunctionComponent<SVGOverlayTypes.Props> = ({map, data, isZoom
       data
   );
   
+  const mapBounds = map.getBounds().pad(0.2);
+  const isProjectedPointVisible = (d: ProjectedLayout<BikeCollision>) => mapBounds.contains({
+    lat: d.d.Latitude,
+    lng: d.d.Longitude
+  });
+  
   const filter = (d: BikeCollision) => d.Severity === CollisionSeverity.fatal;
   
   const markerData = projectedData.filter(d => filter(d.d));
   const contextData = projectedData.filter(d => !filter(d.d));
   
-  const binRadius = getMetersInPixels(map, 300);
+  const projectionXExtent = extent(projectedData, d => d.x) as [number, number];
+  const binRadius = (projectionXExtent[1] - projectionXExtent[0]) / 200;
   
   const relativeBinPoint = projectedData[0];
-  
   
   const hexbinGenerator = hexbin<ProjectedLayout<BikeCollision>>()
       .x(d => d.x - relativeBinPoint.x)
       .y(d => d.y - relativeBinPoint.y)
       .radius(binRadius);
   
-  const hexbinData = hexbinGenerator
-      (contextData);
+  const hexbinData = hexbinGenerator(contextData.filter(isProjectedPointVisible));
   
-  console.log("hexbinData", hexbinData);
+  const maxHexBinValue = max(hexbinData, b => b.length) as number;
   
   const hexbinColorScale = scaleLinear<string, string>()
-      .domain(extent(hexbinData, bin => bin.length) as [number, number])
-      .range(['#FFFFFF00', '#B2222277'])
+      .domain([0, maxHexBinValue])
+      .range(['#FFFFFF00', '#B2222277']);
   
+  
+  const visibleHexBinData = hexbinData.filter(b => b.some(d => isProjectedPointVisible(d)));
+  const visibleMarkerData = markerData.filter(d => isProjectedPointVisible(d));
   return <svg className={styles.svg}>
     <g className={`${isZooming ? styles.zooming : ''}`}>
       {
-        hexbinData.map((bin, i) => {
-          return <g className={styles.hexbin} key={i} transform={`translate(${bin.x},${bin.y})`}>
+        visibleHexBinData.map((bin) => {
+          const key = bin.map(d=>d.d["Accident Index"]).join();
+          
+          return <g className={styles.hexbin} key={key} transform={`translate(${bin.x},${bin.y})`}>
             <g transform={`translate(${relativeBinPoint.x},${relativeBinPoint.y})`}>
               <path fill={hexbinColorScale(bin.length)} d={hexbinGenerator.hexagon(binRadius)}/>
             </g>
-          </g>
+          </g>;
         })
       }
-      {markerData.map(l => {
-        const color = {
-          [CollisionSeverity.fatal]: 'firebrick',
-          [CollisionSeverity.serious]: 'gold',
-          [CollisionSeverity.slight]: 'mediumseagreen'
-        };
-        return <g key={`${l.d["Accident Index"]}`}>
-          <circle cx={l.x} cy={l.y} r={2} fill={color[l.d.Severity]}/>
-        </g>;
-      })}
+      {
+        visibleMarkerData.map(l => {
+          const color = {
+            [CollisionSeverity.fatal]: 'firebrick',
+            [CollisionSeverity.serious]: 'gold',
+            [CollisionSeverity.slight]: 'mediumseagreen'
+          };
+          return <g key={`${l.d["Accident Index"]}`}>
+            <circle cx={l.x} cy={l.y} r={2} fill={color[l.d.Severity]}/>
+          </g>;
+        })
+      }
     </g>
   </svg>;
 };
 
-export default SVGOverlay
+export default SVGOverlay;
