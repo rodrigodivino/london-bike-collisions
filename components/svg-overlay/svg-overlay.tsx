89e700078cc791c4d906751f15bcba02438bba72
Rodrigo Divino <rodrigo.sad.lima@gmail.com>
2022-02-17 10:33:19 -0300

    Complete memoization of SVGOverlay

--- components/svg-overlay/svg-overlay.tsx
+++ components/svg-overlay/svg-overlay.tsx
@@ -1,4 +1,4 @@
-import {FunctionComponent, useCallback, useLayoutEffect, useMemo, useState} from "react";
+import {FunctionComponent, memo, useCallback, useEffect, useLayoutEffect, useMemo, useState} from "react";
 import {SVGOverlayTypes} from "./svg-overlay.types";
 import styles from './svg-overlay.module.css';
 import {CollisionSeverity} from "../../types/collision-severity";
@@ -8,13 +8,7 @@ import {hexbin} from "d3-hexbin";
 import {extent, max, scaleLinear} from "d3";
 import {LatLngBounds} from "leaflet";
 
-const TMP_IS_VISIBLE = (d: ProjectedLayout<BikeCollision>, mapBounds: LatLngBounds) => mapBounds.contains({
-  lat: d.d.Latitude,
-  lng: d.d.Longitude
-});
-
 const SVGOverlay: FunctionComponent<SVGOverlayTypes.Props> = ({map, data, isZooming}) => {
-  
   const [projectedData, setProjectedData] = useState<ProjectedLayout<BikeCollision>[]>([]);
   
   useLayoutEffect(() => {
@@ -30,33 +24,33 @@ const SVGOverlay: FunctionComponent<SVGOverlayTypes.Props> = ({map, data, isZoom
   
   const projectionXExtent = useMemo(() => extent(projectedData, d => d.x) as [number, number], [projectedData]);
   
-  const relativeBinPoint = projectedData[0] ?? {x: 0, y: 0};
-  const binRadius = (projectionXExtent[1] - projectionXExtent[0]) / 200;
+  const binRadius = (projectionXExtent[1] - projectionXExtent[0]) / 100;
   
   const hexbinData = useMemo(() => {
-    return hexbin<ProjectedLayout<BikeCollision>>()
+    const relativeBinPoint = contextData[0] ?? {x: 0, y: 0};
+  
+    const binData = hexbin<ProjectedLayout<BikeCollision>>()
         .x(d => d.x - relativeBinPoint.x)
         .y(d => d.y - relativeBinPoint.y)
         .radius(binRadius)
         (contextData);
-  }, [contextData, binRadius, relativeBinPoint.x, relativeBinPoint.y]);
+    
+    binData.forEach(bin => {
+      bin.x += relativeBinPoint.x;
+      bin.y += relativeBinPoint.y;
+    })
+    
+    return binData;
+  }, [contextData, binRadius]);
   
   
-  const maxHexBinValue = max(hexbinData, b => b.length) as number;
-  
-  const hexbinColorScale = scaleLinear<string, string>()
-      .domain([0, maxHexBinValue])
-      .range(['#FFFFFF00', '#B2222277']);
-  const s = performance.now();
-  
-  
-  const visibleHexBinData = hexbinData//;.filter(b => b.some(d => TMP_IS_VISIBLE(d, map.getBounds())));
-  const visibleMarkerData = markerData//;.filter(d => TMP_IS_VISIBLE(d, map.getBounds()));
+  const hexbinColorScale = useMemo(() => {
+    return  scaleLinear<string, string>()
+        .domain([0, max(hexbinData, b => b.length) as number])
+        .range(['#FFFFFF00', '#B2222277']);
+  }, [hexbinData])
   
   
-  const e = performance.now();
-  
-  console.log('main: ', (e - s) + 'ms');
   
   const projectionOrigin = map.project(map.getBounds().getNorthWest());
   
@@ -65,32 +59,11 @@ const SVGOverlay: FunctionComponent<SVGOverlayTypes.Props> = ({map, data, isZoom
       <g className={`${isZooming ? styles.zooming : ''}`}>
         
         <g className="context">
-          {
-            visibleHexBinData.map((bin) => {
-              const key = bin.map(d => d.d["Accident Index"]).join();
-              
-              return <g className={styles.hexbin} key={key} transform={`translate(${bin.x},${bin.y})`}>
-                <g transform={`translate(${relativeBinPoint.x},${relativeBinPoint.y})`}>
-                  <path fill={hexbinColorScale(bin.length)} d={hexbin().hexagon(binRadius)}/>
-                </g>
-              </g>;
-            })
-          }
+          <MemoizedHexBin hexBinData={hexbinData} hexbinColorScale={hexbinColorScale} binRadius={binRadius}/>
         </g>
         
         <g className="marker">
-          {
-            visibleMarkerData.map(l => {
-              const color = {
-                [CollisionSeverity.fatal]: 'firebrick',
-                [CollisionSeverity.serious]: 'gold',
-                [CollisionSeverity.slight]: 'mediumseagreen'
-              };
-              return <g key={`${l.d["Accident Index"]}`}>
-                <circle cx={l.x} cy={l.y} r={2} fill={color[l.d.Severity]}/>
-              </g>;
-            })
-          }
+          <MemoizedMarker markerData={markerData}/>
         </g>
       </g>
     </g>
@@ -98,3 +71,39 @@ const SVGOverlay: FunctionComponent<SVGOverlayTypes.Props> = ({map, data, isZoom
 };
 
 export default SVGOverlay;
+
+
+const HexBin = ({hexBinData, hexbinColorScale, binRadius}: any): JSX.Element => {
+  return <>
+    {
+      hexBinData.map((bin: any) => {
+        const key = bin.map((d: any) => d.d["Accident Index"]).join();
+        
+        return <g className={styles.hexbin} key={key} transform={`translate(${bin.x},${bin.y})`}>
+          <path fill={hexbinColorScale(bin.length)} d={hexbin().hexagon(binRadius)}/>
+        </g>;
+      })
+    }</>
+};
+
+const Marker = ({markerData}: any): JSX.Element => {
+  return <>
+    {
+      markerData.map((l: any) => {
+        const color: Record<string, string> = {
+          [CollisionSeverity.fatal]: 'firebrick',
+          [CollisionSeverity.serious]: 'gold',
+          [CollisionSeverity.slight]: 'mediumseagreen'
+        };
+        
+        return <g key={`${l.d["Accident Index"]}`}>
+          <circle cx={l.x} cy={l.y} r={2} fill={color[l.d.Severity]}/>
+        </g>;
+      })
+    }</>
+}
+
+const MemoizedHexBin = memo(HexBin);
+const MemoizedMarker = memo(Marker);
+
+
