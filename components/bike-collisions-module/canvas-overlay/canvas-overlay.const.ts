8f2c3ef772111379ca190f5c9a882a05a9abe487
Rodrigo Divino <rodrigo.sad.lima@gmail.com>
2022-02-23 12:06:24 -0300

    Add TODOs for next steps

--- components/bike-collisions-module/canvas-overlay/canvas-overlay.const.ts
+++ components/bike-collisions-module/canvas-overlay/canvas-overlay.const.ts
@@ -1,4 +1,11 @@
 export namespace CanvasOverlayConst {
   export const COLOR_THRESHOLD_STEP = 10;
+  /*
+   TODO: Revert radius factor back to 300 and go with the blur approach
+   TODO: Keep the full data on the context
+   TODO: Add back blur
+   TODO: Filter hexagons below a given threshold
+   TODO: Decide how to place markers
+   */
   export const BIN_RADIUS_FACTOR = 1200;
 }
