fdefec609f530edfa2e6044e66b1ba72fa858009
Rodrigo Divino <rodrigo.sad.lima@gmail.com>
2022-02-25 09:09:25 -0300

    Update tile layer

--- hooks/use-map-initialization.ts
+++ hooks/use-map-initialization.ts
@@ -16,10 +16,13 @@ export const useMapInitialization = (
     
     mapRef.current = L.map(mapContainerRef.current).setView(initialCenter, initialZoom);
   
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       subdomains: 'abcd',
-      maxZoom: 20
+      minZoom: 0,
+      maxZoom: 20,
+      //@ts-ignore
+      ext: 'png'
     }).addTo(mapRef.current);
     
   }, [mapContainerRef, initialZoom, initialCenter]);
