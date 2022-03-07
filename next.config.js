2768ee0351ceffd99cbfc32bf8c7e15e705ff7c3
Rodrigo Divino <rodrigo.sad.lima@gmail.com>
2022-03-07 11:44:40 -0300

    Remove basePath and asset prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  basePath: '/london-bike-collisions',
-  assetPrefix: '/london-bike-collisions',
+  // basePath: '/london-bike-collisions',
+  // assetPrefix: '/london-bike-collisions',
 }
