b9274d1a3d048813703949259204e32cac601790
Rodrigo Divino <rodrigo.sad.lima@gmail.com>
2022-03-08 11:30:01 -0300

    Update asset prefix

--- next.config.js
+++ next.config.js
@@ -3,5 +3,5 @@
 module.exports = {
   reactStrictMode: true,
   basePath: process.env.NODE_ENV === 'development' ? '' : '/london-bike-collisions',
-  assetPrefix: process.env.NODE_ENV === 'development' ? '' : '/london-bike-collisions',
+  assetPrefix: process.env.NODE_ENV === 'development' ? '' : '/london-bike-collisions/',
 }
