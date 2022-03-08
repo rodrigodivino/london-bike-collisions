66a64c053002e3ec96e3fb5ac1b2965a1b76c45e
Rodrigo Divino <rodrigo.sad.lima@gmail.com>
2022-03-08 12:00:58 -0300

    Add different path for data on build

---
+++ out/_next/static/IfwFp6ThmyvrnnFwDc25i/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/561-a954654e881c7d23.js","static/css/56cb32ecd583e970.css","static/chunks/pages/index-05aac87231b887cf.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
